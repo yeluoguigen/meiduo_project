4b75c18e429855642a08726d6ac03c6877fb85a0
yck <yechuanku@163.com>
2019-05-23 14:20:28 +0800

    7-配置本地域名

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'mfjmh6=8ey(x2vo75^i@qik1vmlvyjwaf*59e8((065d%8ur6c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.meiduo.site']
 
 
 # Application definition
